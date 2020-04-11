a7a739f1f22bab8a48aaec10a6164ad9e76698fc
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-10 23:31:09 -0700

    add some proprieties to github profile

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,6 +10,9 @@ const card = props => {
       <div className={classes.Info}>
         <h3> {profile.name} </h3>
         <p> {profile.location}</p>
+        <p> {profile.public_repos} </p>
+        <p> {profile.followers} </p>
+        <p> {profile.following} </p>
       </div>
     </div>
   );
